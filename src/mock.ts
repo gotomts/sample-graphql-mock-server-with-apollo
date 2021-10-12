6f59994dd056d576ea26ceb02922ba26bed2aa84
Mitsuharu Goto <mh.goto.web@gmail.com>
2021-10-13 08:04:54 +0900

    feat: モックのカスタマイズ

--- src/mock.ts
+++ src/mock.ts
@@ -1,14 +1,41 @@
 import { ApolloServer, gql } from 'apollo-server';
+import * as casual from 'casual';
 
 const typeDefs = gql`
+  type Person {
+    name: String
+    age: Int
+  }
+
   type Query {
+    persons: [Person]
     hello: String
+    resolved: String
   }
 `;
 
+const resolvers = {
+  Query: {
+    resolved: () => 'Resolved',
+  },
+};
+
+const mocks = {
+  Int: () => 6,
+  Float: () => 22.1,
+  String: () => 'Hello',
+  Person: () => ({
+    name: casual.name,
+    age: () => casual.integer(0, 120),
+    friends: [...new Array(casual.integer(2, 6))],
+    listOfLists: () => [...new Array(3)].map((i) => [...new Array(2)]),
+  }),
+};
+
 const server = new ApolloServer({
   typeDefs,
-  mocks: true,
+  resolvers,
+  mocks,
 });
 
 server.listen().then(({ url }) => {
